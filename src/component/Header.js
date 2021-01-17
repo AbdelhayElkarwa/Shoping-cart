9b210f36ca580383cd2300cf88bc095df3605166
Abdelhay Elkarwa <elkarwaa@gmail.com>
2021-01-17 21:09:05 +0200

    first comment

---
+++ src/component/Header.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Header extends Component {
+    render() {
+        return (
+            <div>
+                <h3>Shoping Cart</h3>
+            </div>
+        )
+    }
+}
+
+export default Header
